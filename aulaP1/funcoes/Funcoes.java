0c1bcd8a97b398745aeeef9ace0fabaade540a9c
tgmoraes <tiago.moraes@osorio.ifrs.edu.br>
2020-10-28 21:27:45 -0300

    alteracoes feitas pelos exemplos implementados em P1 em Funcoes.java

--- aulaP1/funcoes/Funcoes.java
+++ aulaP1/funcoes/Funcoes.java
@@ -1,25 +1,59 @@
 package aulaP1.funcoes;
 
+import java.util.Scanner;
+
 public class Funcoes {
 	static int soma(int a, int b) {
 		return a+b;
 	}
+	//multi realiza a multiplicacao inteira de a por b
+	//a é um inteiro
+	//b é um inteiro
 	static int multi(int a, int b) {
+		if(a==0 || b==0) {
+			return 0;
+		}
 		int res = a;
-		for(int i=b;i>1;i--) res= soma(a,res);
+		for (int i = b; i > 1; i = soma(i, -1)) {
+			res = soma(a, res);
+		}
 		return res;
 	}
+	static String ola() {
+		return "ola mundo";
+	}
+	
+	static void printaVet(int[] vet) {
+		//implementacao da funcao
+		//foreach// para cada elemento de vet faça
+		for (int elemento : vet) {
+			System.out.print(elemento+" ");
+		}
+	}
+	
 	public static void main(String[] args) {
+		String frase = ola();
 		//testes de soma
 		System.out.println(soma(5,10)==15);
 		System.out.println(soma(5,-10)==-5);
+		System.out.println(soma(5,0)==5);
+		System.out.println(soma(-5,0)==-5);
 
 		//teste de multi
 		System.out.println(multi(5,10)==50);
 		System.out.println(multi(10,5)==50);
-
-		//System.out.println(multi(10,0)==0);
-		//System.out.println(multi(0,5)==0);
-
+		System.out.println(multi(10,0)==0);
+		System.out.println(multi(0,5)==0);
+		System.out.println(multi(10,-5)==-50);
+		System.out.println(multi(-10,-5)==50);
+		System.out.println(multi(-10,5)==-50);
+		
+		int[] vetor = {3,56,7,8,3,23,43,11,98};
+		printaVet(vetor);		
 	}
 }
+
+
+
+//5*4 = 5 + 5 + 5 + 5
+
